d20e40f5b5bfb99d83dbeaad3aaa4960c7c720ce
Akah <robin.white280@gmail.com>
2020-10-23 00:18:53 +0200

    calculate fps

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "config.h"
+/* #include "config.h" */
 #include "main.h"
 
 int main()
@@ -17,9 +17,12 @@ int main()
 void main_loop(SDL_Renderer* pRenderer)
 {
     SDL_Event event;
+    uint fps_last = SDL_GetTicks();
+    uint fps_current = 0;
+    uint fps_frames = 0;
     int render_timer = roundf(1000.0f / 60.0f);
     const int delta_time = roundf(1000.0f / 60.0f);
-    
+
     while (!quit) {
 	const int start_ticks = SDL_GetTicks();
 
@@ -30,13 +33,19 @@ void main_loop(SDL_Renderer* pRenderer)
 	render_timer -= delta_time;
 
 	if (render_timer <= 0) {
-	    printf("quit=%d\n", quit);
 	    render(pRenderer);
 	    render_timer = delta_time;
 	}
 
-	int end_ticks = SDL_GetTicks();
+	fps_frames++;
+	printf("%d\n", fps_current);
+	if (fps_last < SDL_GetTicks() - 1000) {
+	    fps_last = SDL_GetTicks();
+	    fps_current = fps_frames;
+	    fps_frames = 0;
+	}
 
+	int end_ticks = SDL_GetTicks();
 	SDL_Delay(delta_time - (end_ticks - start_ticks));
     }
 }
