adab13e015b61e0d73c41dcf844afcfa153329d6
Akah <robin.white280@gmail.com>
2020-10-27 22:31:01 +0100

    move asset related files into directory

--- src/common.h
+++ src/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "asset/assets.h"
 #include "state.h"
-#include "assets.h"
 
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
--- src/main.h
+++ src/main.h
@@ -2,6 +2,6 @@
 #include "init/init.h"
 #include "draw/draw.h"
 #include "input/input.h"
-#include "asset_manager.h"
+#include "asset/asset_manager.h"
 
 void main_loop(SDL_Renderer *pRenderer, State *pState, AssetManager *pAssetManager);
