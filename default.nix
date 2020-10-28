10010f48fa3fa2f6668447840e23b9893daf5d68
Akah <robin.white280@gmail.com>
2020-10-28 22:36:40 +0100

    added nix shell file

---
+++ default.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {}; {
+    pkmnEnv = gcc8Stdenv.mkDerivation {
+        name = "pkmn-env";
+        buildInputs = [
+	    pkg-config
+	    stdenv
+	    gcc
+	    SDL2
+	    SDL2_image
+	    gnumake
+	    automake
+	    autoconf
+	];
+    };
+}
+
