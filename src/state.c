0f950a93c71a3677fcd622be765ba53583f7e823
Akah <robin.white280@gmail.com>
2021-01-17 00:24:12 +0000

    debug set state

--- src/state.c
+++ src/state.c
@@ -1,9 +1,13 @@
 #include "common.h"
 #include "state.h"
+#include "utils.h"
 
 void set_current_state(enum states new_state)
 {
-    printf("old state %d\n", state->current_state);
+    char current_state_string[64];
+    sprintf(current_state_string, "old state %d\n", state->current_state);
+    _utils_debug(current_state_string);
     state->current_state = new_state;
-    printf("current state %d\n", state->current_state);
+    sprintf(current_state_string, "current state %d\n", state->current_state);
+    _utils_debug(current_state_string);
 }
